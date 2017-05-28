7a17b7ea42e7342057767facf50d09cbfb737d4e
Hariharan <theroboticlabs@gmail.com>
2017-05-28 13:13:37 +0530

    Added a 15 seconds delay in the while loop.

--- autodeleter.py
+++ autodeleter.py
@@ -1,4 +1,4 @@
-import pymysql,requests
+import pymysql,requests,time
 db = pymysql.connect("localhost","root","10sa1017213","Offline")
 cursor = db.cursor()
 i=0;
@@ -11,16 +11,13 @@ while cursor.execute("SELECT * FROM files WHERE 1"):
     print(results);
     for row in results:
         ip = row[2]
-        if ip not in ips:
-            ips.append(ip)
-            port = row[8]
-            username = row[6]
-            password = row[7]
-            try:
-                r = requests.get("http://"+username+":"+password+"@"+ip+":"+port)
-            except requests.exceptions.RequestException as e:
-                cursor.execute('DELETE FROM files WHERE ip = \'%s\''%ips[i])
-                db.commit()
+        port = row[8]
+        username = row[6]
+        password = row[7]
+        try:
+            r = requests.get("http://"+username+":"+password+"@"+ip+":"+port)
+        except requests.exceptions.RequestException as e:
+            cursor.execute('DELETE FROM files WHERE ip = \'%s\''%ip)
+            db.commit()
         print("IP:"+ip+", Port:"+port)
-        i = i+1
-    
+        time.sleep(15)
