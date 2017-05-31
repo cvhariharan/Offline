cb2d53940c73ea02b379c83bdc73566941742537
Hariharan <theroboticlabs@gmail.com>
2017-05-31 16:23:13 +0530

    Added fast hashing of all the sub directories and sub files in the current directory.

--- authenticate.py
+++ authenticate.py
@@ -1,4 +1,4 @@
-import requests, time, threading
+import requests, time, threading,_thread,xxhash
 import hashlib,checksumdir
 import os, sys, socket, json, random, string
 server_conf = open("sr.conf","r")
@@ -8,17 +8,6 @@ server_pass = data[1].strip('Password:').strip('\n')
 port = int(data[2].strip('Port:').strip('\n'))
 localhost = data[3].strip('Localhost:').strip('\n')
 status = 0 #Server is Offline
-"""def lister(dir_name):
-    os.chdir(dir_name)
-    dirs = os.listdir(os.getcwd())
-    for file in dirs:
-        if not file.startswith('authenticate') and not file.startswith('downloader') and not file.startswith('sr') and not file.startswith('basichttpserver') and not file.startswith('.'):
-            if not os.path.isdir(file) and os.path.isfile(file):
-                print(str(file)+":"+md5sum(file))
-            elif os.path.isdir(file):
-                print(str(file)+":"+checksumdir.dirhash(file))
-                lister(file)
-"""
 def my_ip():
     s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     s.connect(('192.0.0.2',1027))
@@ -32,16 +21,6 @@ def downloader():
     com = "python downloader.py"
     os.system(com)
 
-def md5sum(filename):
-  with open(filename, mode='rb') as f:
-    d = hashlib.md5()
-    while True:
-      buf = f.read(4096) # 128 is smaller than the typical filesystem block
-      if not buf:
-        break
-      d.update(buf)
-    return d.hexdigest()
-#Add Server creator to constantly check for server status and start it.
 def server_status():
     try:
         r = requests.get("http://"+server_user+":"+server_pass+"@localhost:"+str(port))
@@ -70,42 +49,17 @@ while auth != 1 and attempts != 0:
     if r.text == '1':
         print ("Authentication Successful!")
         auth = 1
-        #server_conf = open("sr.conf","r")
-        #data = server_conf.readlines()
-        #server_user = data[0].strip('Username:').strip('\n')
-        #server_pass = data[1].strip('Password:').strip('\n')
-        #port = int(data[2].strip('Port:').strip('\n'))
-        #path = "/var/www/html/"
-        #server_user = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
-        #server_pass = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
-        #print(server_user)
-        #print(server_pass)
-        #port = random.randint(6000,7000)
-        #dirs = os.listdir(os.getcwd())
-        #str_dir = str(dirs)
-        #lister(os.getcwd())
-        dir_list = os.listdir(os.getcwd())
-        dir_str = ""
-        for file in dir_list:
-            if not file.startswith('authenticate') and not file.startswith('downloader') and not file.startswith('sr') and not file.startswith('basichttpserver'):
-                if not os.path.isdir(file):
-                    #print(file+":"+md5sum(file))
-                    dir_str = dir_str+","+str(file)+":"+md5sum(file)
-                else:
-                    #print(file+":"+checksumdir.dirhash(file))
-                    dir_str = dir_str+","+str(file)+":"+checksumdir.dirhash(file)
-
+        dir_str = " "
+        os.system("python3 directory_lister.py")
+        file_dat = open("files.dat","r")
+        all_files = file_dat.readlines()
+        for files in all_files:
+            dir_str = dir_str + "," + (files.replace(str(os.getcwd()),""))
         response = requests.post("http://"+localhost+"/direcctorylist.php", data = {'list':dir_str,'username':username,'ipaddr':ipaddr,'location':os.getcwd(),'server_user':server_user,'server_pass':server_pass,'port':port})
+        os.remove("files.dat")
         #downloader()
-
+        #print(dir_str)
         downloader()
-        #downloader()
-        #print("Response: \n")
-        #print(response.text)
-        #server_command = "python basichttpserver.py " + str(port) + " " + server_user + ":" + server_pass
-        #os.system(server_command)
-    #for file in dirs:
-    #    print (file)
     elif attempts != 0:
         if attempts != 1:
             print("Authentication Failed! Try Again.")
--- directory_lister.py
+++ directory_lister.py
@@ -1,22 +1,30 @@
-#Test to see recursive directory lister
-import os,hashlib,checksumdir,glob,_thread,xxhash
+import os,hashlib,checksumdir,glob,_thread,xxhash,json
+#from datetime import datetime
+#The following function does not generate md5 hash. It uses xxhash. The entire file is not hashed. Only 25 chunks of 8k from the middle is hashed.
 def md5sum(filename):
   with open(filename, mode='rb') as f:
     d = xxhash.xxh64()
-    while True:
-      buf = f.read(8192) # 128 is smaller than the typical filesystem block
-      if not buf:
-        break
-      d.update(buf)
+    i=0;
+    mid = int((os.path.getsize(filename))/2)
+    while i < 25:
+        f.seek(mid)
+        buf = f.read(8192) # 128 is smaller than the typical filesystem block
+        if not buf:
+            break
+        d.update(buf)
+        i+=1
     return str(d.hexdigest()).strip("b").strip("\'")
-    dir_str = " "
-def dir_hash(file):
-    print(checksumdir.dirhash(file))
-def file_hash(file):
-    print(file)
-    print(md5sum(file))
-
-dir_str = " "
+f = open("files.dat","w")
+def file_hash(dir_name):
+    f.write(dir_name+":"+md5sum(dir_name)+'\r\n')
+def dir_hash(dir_name):
+    hashes = ""
+    for root, dirs, files in os.walk(dir_name, topdown=False):
+        for name in files:
+            hashes = hashes+md5sum(os.path.join(root,name))
+    hashes = str(xxhash.xxh64(hashes).hexdigest()).strip("b").strip("\'")
+    last_modified = os.path.getmtime(dir_name)
+    f.write(dir_name+":"+hashes+'\r\n')
 def lister(dir_name):
     for root, dirs, files in os.walk(dir_name, topdown=False):
         for name in files:
@@ -24,10 +32,8 @@ def lister(dir_name):
             _thread.start_new_thread(file_hash,(os.path.join(root,name),))
         for name in dirs:
             #dir_str = dir_str + os.path.join(root, name)
-            print(os.path.join(root, name))
-
-all_files = []
-#dir_name = input("Directory: ")
-lister('/run/media/hariharan/OS/Series')
-print(dir_str)
-#print(all_files)
+            #last_modified = os.path.getmtime(os.path.join(root, name))
+            #f.write(name+":"+str(last_modified)+",")
+            dir_hash(os.path.join(root,name))
+    f.close()
+lister(os.getcwd())
