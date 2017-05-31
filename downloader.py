13e36f9d2ef0198c963bf4ab04161dc3a2a80948
Hariharan <theroboticlabs@gmail.com>
2017-05-31 16:23:53 +0530

    Small changes to make it work with new name format.

--- downloader.py
+++ downloader.py
@@ -39,7 +39,7 @@ def decodeAnddownload(fname):
         name = urllib.quote(name)
         server = dns(servername)
         print(server)
-        download_com = "wget http://"+server_user+":"+server_pass+"@"+server+":"+port+"/"+name
+        download_com = "wget http://"+server_user+":"+server_pass+"@"+server+":"+port+name
         #print(download_com)
         os.system(download_com)
         #print(name)
