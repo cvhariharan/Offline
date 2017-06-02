f03c6bd788c574324984e9ab8716cbbb1584f3b4
Hariharan <theroboticlabs@gmail.com>
2017-06-02 19:47:22 +0530

    Ability to run continuously added.

--- autodeleter.py
+++ autodeleter.py
@@ -5,19 +5,20 @@ i=0;
 ips =  []
 row = []
 
-
-while cursor.execute("SELECT * FROM files WHERE 1"):
-    results = cursor.fetchall()
-    print(results);
-    for row in results:
-        ip = row[2]
-        port = row[8]
-        username = row[6]
-        password = row[7]
-        try:
-            r = requests.get("http://"+username+":"+password+"@"+ip+":"+port)
-        except requests.exceptions.RequestException as e:
-            cursor.execute('DELETE FROM files WHERE ip = \'%s\''%ip)
-            db.commit()
-        print("IP:"+ip+", Port:"+port)
+while True:
+    while cursor.execute("SELECT * FROM files WHERE 1"):
+        results = cursor.fetchall()
+        print(results);
+        for row in results:
+            ip = row[2]
+            port = row[8]
+            username = row[6]
+            password = row[7]
+            try:
+                r = requests.get("http://"+username+":"+password+"@"+ip+":"+port)
+            except requests.exceptions.RequestException as e:
+                cursor.execute('DELETE FROM files WHERE ip = \'%s\''%ip)
+                db.commit()
+            print("IP:"+ip+", Port:"+port)
+        time.sleep(10)
     time.sleep(15)
