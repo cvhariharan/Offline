27224b5bb115938130358f15523ee668f6bee1fe
Hariharan <theroboticlabs@gmail.com>
2017-06-14 14:10:17 +0530

    First attempt of the search page. No responsive scaling.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Offl!ne.</title>
+  <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">
+  <link href="index.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<div class="col-5"></div>
+<div class="col-7"><h2>Offl!ne.</h2></div>
+<div id="search_box">
+<form method="POST" action="search.php">
+  <div class="col-4"></div>
+<div class="col-8"><input type="text" name="query"></div>
+</form>
+</div>
+
+</body>
+
+</html>
