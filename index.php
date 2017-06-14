e265994781916d717f4e5b8aefe2a3750c40e46a
Hariharan <theroboticlabs@gmail.com>
2017-06-14 16:34:46 +0530

    Basic Responsiveness added.

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
-<html>
+ <html>
 <head>
-  <title>Offl!ne.</title>
+  <title>Offl!ne</title>
   <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">
   <link href="index.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-
-<div class="col-5"></div>
-<div class="col-7"><h2>Offl!ne.</h2></div>
+<div class="navbar">
+<ul align="right">
+  <li><a href="login.php">Login</a></li>
+  <li><a href="signup.php">Signup</a></li>
+</ul>
+</div>
+<div class="offline">
+<h2>Offl!ne.</h2></div>
 <div id="search_box">
 <form method="POST" action="search.php">
-  <div class="col-4"></div>
-<div class="col-8"><input type="text" name="query"></div>
+<center><input type="text" name="query"></center>
 </form>
 </div>
-
 </body>
-
 </html>
