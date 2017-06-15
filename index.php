b6b682fb006f0ffe58e976d89236894db214c741
Hariharan <theroboticlabs@gmail.com>
2017-06-15 12:24:23 +0530

    Visual Improvements and Bug Fixes.

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
  <html>
 <head>
   <title>Offl!ne</title>
-  <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Overpass" rel="stylesheet">
   <link href="index.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -30,7 +28,7 @@
 
 </div>
 <div class="offline">
-<h2>Offl!ne.</h2></div>
+<p>Offl!ne.</p></div>
 <div id="search_box">
 <form method="POST" action="search.php">
 <center><input type="text" name="query"></center>
--- search.php
+++ search.php
@@ -1,8 +1,15 @@
-<form method="POST" action "">
-<input type="text" name="query" placeholder="Search" required>
-<input type="submit" name="submit">
-</form>
+<html>
+<head>
+ <title>Offl!ne</title>
+ <link href="index.css" rel="stylesheet">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<center><form method="POST" action "">
+<div class="heading"><label>Offl!ne</label></div>
+<input type="text" name="query" required>
 
+</form></center>
 <?php
 require 'database.php';
 session_start();
@@ -12,20 +19,18 @@ $query = $_POST['query'];
 $ipaddr = $_SERVER['REMOTE_ADDR'];
 $sql = "SELECT * FROM files WHERE MATCH (name,location) AGAINST(\"$query\") ORDER BY MATCH (name) AGAINST('$query') DESC";
 $result = mysqli_query($link,$sql);
-/*$row = mysqli_fetch_array($result);
-print_r($row);*/
-if(!empty($row))
-{
 while($row = mysqli_fetch_array($result))
 {
   $name = $row['name'];
   $hash = $row['hash'];
   $host = $row['username'];
-  echo "From: $host <a href=\"http://192.168.0.8/Offline/postman.php?hash=$hash\">$name</a> <br>";
+  echo "<div class=\"links\">From: $host <a href=\"http://192.168.0.8/Offline/postman.php?hash=$hash\">$name</a></div><br>";
 }
 }
-}
-else {
+else
+{
   echo "<a href=\"login.php\">Login</a>";
 }
- ?>
+?>
+</body>
+</html>
