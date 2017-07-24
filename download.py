241508b14774f45d579a5f55854d21b26993935c
Hariharan <theroboticlabs@gmail.com>
2017-07-24 08:06:23 +0530

    Command line url handling.

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
-import os
+import os,sys
 from pySmartDL import SmartDL
-url = input("URL: ")
+
+url = str(sys.argv[1]) #The first argument is the name of the python script
+#url = input("URL: ")
+#print(url)
 dest = os.getcwd()
 
 obj = SmartDL(url,dest)
 obj.start()
 
 path = obj.get_dest()
-
