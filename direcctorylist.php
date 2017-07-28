f36ceaa01bef578a3c6a8167bdaf2ed86ecbb447
Hariharan <theroboticlabs@gmail.com>
2017-07-28 20:45:01 +0530

    Directorylist php updated to work with new lister python.

--- direcctorylist.php
+++ direcctorylist.php
@@ -1,28 +1,25 @@
 <?php
-set_time_limit(0);
 require "database.php";
-$json_data = $_POST['list'];
-$username = $_POST['username'];
-$ipaddr = $_POST['ipaddr'];
-$location = $_POST['location'];
-$server_user = $_POST['server_user'];
-$server_pass = $_POST['server_pass'];
-$port = $_POST['port'];
-$json_data = str_replace(array('\'','[',']'), '', $json_data);
-$files = explode(",",$json_data);
-echo "<pre>";
-print_r($files);
-echo "</pre>";
+$d = file_get_contents("php://input");
+$json = json_decode($d, true);
+$all_keys  = array_keys($json);
+$user_data = $json["me"];
+$username = $user_data["username"];
+$port = $user_data["port"];
+$server_user = $user_data["server_user"];
+$server_pass = $user_data["server_pass"];
+$ipaddr = $user_data["ipaddr"];
 $delete = "DELETE FROM files WHERE ip = \"$ipaddr\"";
 $del_result = mysqli_query($link,$delete);
-foreach ($files as $file)
+foreach ($all_keys as $key)
 {
-  $file_split = explode(":",$file);
-  $hash =  $file_split[1];
-  $loc = $file_split[0];
-  $name_ = explode("/",$file_split[0])
-  $name = $name_(count($name_)-1)
-  $sql = "INSERT INTO `files`(`name`, `location`, `ip`, `username`, `server_user`, `server_pass`, `port`,`hash`) VALUES (\"$name\",\"$loc\",\"$ipaddr\",\"$username\",\"$server_user\",\"$server_pass\",\"$port\",\"$hash\")";
+  if(isset($json[$key]["name"]))
+  {
+  $name = $json[$key]["name"];
+  $hash = $json[$key]["hash"];
+  $sql = "INSERT INTO `files`(`name`, `location`, `ip`, `username`, `server_user`, `server_pass`, `port`,`hash`) VALUES (\"$name\",\"$key\",\"$ipaddr\",\"$username\",\"$server_user\",\"$server_pass\",\"$port\",\"$hash\")";
+  //echo $sql;
   $result = mysqli_query($link,$sql);
+}
 }
 ?>
--- json_test.php
+++ json_test.php
@@ -1,10 +1,12 @@
 <?php
+
 $d = file_get_contents("php://input");
-$j = json_decode($d,true);
-//$d = fread($jd,filesize("files.json"));
-//$data = json_decode(trim($d),true);
-foreach(array_keys($j) as $arr)
-{
-  echo $arr;
-}
+$json = json_decode($d, true);
+$user_data = $json["me"];
+$name = $user_data["name"];
+$username = $user_data["username"];
+$port = $user_data["port"];
+$server_user = $user_data["server_user"];
+$server_pass = $user_data["server_pass"];
+
 ?>
