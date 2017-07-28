98d409371d68c7c6d049cacfdda0e94c4c82f77a
Hariharan <theroboticlabs@gmail.com>
2017-07-28 20:52:35 +0530

    Updated configurations.

--- database.php
+++ database.php
@@ -1,13 +1,8 @@
 <?php
+/*Testing server configurations*/
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'admin';
 $link = mysqli_connect($dbhost,$dbuser,$dbpass);
-/*if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}*/
 mysqli_select_db($link,'Offline');
 ?>
