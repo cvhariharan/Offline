320f69089dea23749c2c8248a9ba5364ce0249c7
Hariharan <theroboticlabs@gmail.com>
2017-08-06 14:32:28 +0530

    Changed credentials.

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 /*Testing server configurations*/
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = 'admin';
+$dbpass = '';
 $link = mysqli_connect($dbhost,$dbuser,$dbpass);
 mysqli_select_db($link,'Offline');
 ?>
