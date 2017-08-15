e978856651e8c65c9dec258846e1fa2b21db583c
Hariharan <theroboticlabs@gmail.com>
2017-08-15 19:33:59 +0530

    Updated localhost address.

--- sr_generator.php
+++ sr_generator.php
@@ -6,7 +6,7 @@ function random_string($length)
 $server_user = random_string(6);
 $server_pass = random_string(8);
 $port = rand(6000,7500);
-$localhost = "10.10.6.9";
+$localhost = "10.10.9.5";
 header("Content-type: text/plain");
 header("Content-Disposition: attachment; filename=sr.conf");
 $content = "Username:$server_user\nPassword:$server_pass\nPort:$port\nLocalhost:$localhost\n";
