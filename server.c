68fba732a5a46659a990d32cf86ce8faa71d05b3
Hariharan <theroboticlabs@gmail.com>
2017-08-18 21:50:54 +0530

    Just a little code to keep the server running.

---
+++ server.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void main(int argc, char* argv[])
+{
+    char *cmd;
+    cmd = (char *)malloc(100*sizeof(char));
+    if(argc>=3)
+    {
+        strcpy(cmd,"Server-win.exe --username ");
+        strcat(cmd, argv[1]);
+        strcat(cmd, " --password ");
+        strcat(cmd, argv[2]);
+        strcat(cmd , " -p ");
+        strcat(cmd, argv[3]);
+        strcat(cmd, " > Server.log");
+        //printf("%s",cmd);
+    }
+
+    while(1)
+    {
+        system(cmd);
+    }
+}
