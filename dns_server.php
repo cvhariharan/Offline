a856bf4b190d75c7592654e73f874da65249d46e
Hariharan <theroboticlabs@gmail.com>
2017-08-18 16:20:40 +0530

    Delete dns_server.php

--- dns_server.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "database.php";
-$name = $_POST['name'];
-$sql = "SELECT ip FROM users WHERE username = \"$name\"";
-$result = mysqli_query($link,$sql);
-$row = mysqli_fetch_array($result);
-echo $row['ip'];
-?>
