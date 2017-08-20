4b23070d63d9e3b183b8bd435d5eeed02df886d5
Hariharan <theroboticlabs@gmail.com>
2017-08-20 11:38:41 +0530

    Can be used to stop client software of individual users.

--- notification.php
+++ notification.php
@@ -1,1 +1,14 @@
-Welcome to Offline!
+<?php
+$stop_list = array(); //List of usernames whose client software has to be stopped.
+$version = "1.0.0";
+$client_version = $_GET["version"];
+$username = $_GET["username"];
+$message = "Welcome to Offline!:start";
+$stop_message = "Download the latest version.:stop";
+if (!in_array($username,$stop_list))
+{
+	echo $message;
+}
+else
+	echo $stop_message;
+?>
